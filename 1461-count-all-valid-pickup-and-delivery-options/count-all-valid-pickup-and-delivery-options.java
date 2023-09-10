c274a9fa9b331be88ea0cb9fa971be3c7f4bc055
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2023-09-10 23:08:13 +0530

    Time: 0 ms (100.00%) | Memory: 39 MB (80.49%) - LeetSync

---
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int countOrders(int n) {
+        long count = 1,mod=1000000007;
+        for (int i = 2; i <= n; i++) {
+            count = (count * (2 * i - 1) * i) % mod;
+        }
+        return (int) count;
+    }
+}
