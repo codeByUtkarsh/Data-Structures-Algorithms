584c590f9bacd8fd71bc071dfa808cac60c1966c
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2023-09-18 20:40:23 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Subarrays with sum K/subarrays-with-sum-k.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int Arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                Arr[i] = Integer.parseInt(input_line[i]);
+            int k = Integer.parseInt(read.readLine());
+            
+            Solution ob = new Solution();
+            System.out.println(ob.findSubArraySum(Arr, N, k));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static int findSubArraySum(int Arr[], int N, int k)
+    {
+        // code here
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int sum =0;
+        int ans =0;
+        hm.put(0,1);
+        for(int i=0;i<Arr.length;i++){
+            sum+=Arr[i];
+            if(hm.containsKey(sum-k)){
+                ans+=hm.get(sum-k);
+            }
+            hm.put(sum,hm.getOrDefault(sum,0)+1);
+        }
+        return ans;
+    }
+}
