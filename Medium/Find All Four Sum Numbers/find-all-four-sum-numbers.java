e14a6819e4d4ef92da274db22cbb042570a50a60
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2023-09-26 23:52:19 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find All Four Sum Numbers/find-all-four-sum-numbers.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+            String[] nk = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(nk[0]);
+            int k = Integer.parseInt(nk[1]);
+
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+
+            Solution sln = new Solution();
+            ArrayList<ArrayList<Integer>> ans = sln.fourSum(a, k);
+            for (ArrayList<Integer> v : ans) {
+                for (int u : v) {
+                    System.out.print(u + " ");
+                }
+                System.out.print("$");
+            }
+            if (ans.isEmpty()) {
+                System.out.print(-1);
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+// arr[] : int input array of integers
+// k : the quadruple sum required
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> fourSum(int[] arr, int s) {
+        // code here
+       int n=arr.length;
+        Arrays.sort(arr);
+       
+         ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
+         
+         for(int i=0;i<n-3;i++){
+             if(i>0 && arr[i]==arr[i-1])continue;
+             for(int j=i+1;j<n-2;j++){
+                  if(j>i+1 && arr[j]==arr[j-1])continue;
+                 for(int k=j+1;k<n-1;k++){
+                      if(k>j+1 && arr[k]==arr[k-1])continue;
+                     for(int l=k+1;l<n;l++){
+                          if(l>k+1 && arr[l]==arr[l-1])continue;
+                         int sum=arr[i]+arr[j]+arr[k]+arr[l];
+                         
+                         if(sum==s){
+                           ArrayList<Integer> temp=new ArrayList<Integer>(Arrays.asList(arr[i],arr[j],arr[k],arr[l])) ; 
+                            ans.add(temp);
+                         }
+                     }
+                 }
+             }
+         }
+         return ans;
+        
+    }
+}
