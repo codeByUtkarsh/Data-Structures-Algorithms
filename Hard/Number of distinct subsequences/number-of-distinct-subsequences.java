2510c03e445bd9548c878b1b1bfa523bc7ca2ec4
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2023-10-02 23:32:48 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Number of distinct subsequences/number-of-distinct-subsequences.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String s = read.readLine();
+            Solution ob = new Solution();
+            System.out.println(ob.distinctSubsequences(s));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int distinctSubsequences(String s) {
+        // code here
+        int mod = 1000000007;
+        int n = s.length();
+        long arr[] = new long[26];
+        Arrays.fill(arr,-1);
+        
+        long ans = 0;
+        long diff = 0;
+        for(int i=0;i<n;i++){
+            int x = s.charAt(i)-'a';
+            if(i==0){
+                ans=1;
+                arr[x]=1;
+                continue;
+            }
+            
+            diff = 1+ans;
+            
+            if(arr[x]==-1) ans = (ans+diff)%mod;
+            else ans = (ans+diff-arr[x])%mod;
+            arr[x]=diff;
+        }
+        ans+=1;
+        
+        return (int)(ans<0?ans+mod:ans);
+    }
+}
