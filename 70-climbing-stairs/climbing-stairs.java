d8754a543c4bd83a9b6527c2fd463bbe8080ad31
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2023-11-01 00:39:14 +0530

    Time: 0 ms (100.00%) | Memory: 39.4 MB (25.21%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -6,7 +6,7 @@ class Solution {
         if(n<0){
             return 0;
         }
-        if(dp[n]!=0){
+        if(dp[n]!=-1){
             return dp[n];
         }
         dp[n] = countWays(n-1,dp)+countWays(n-2,dp);
@@ -14,6 +14,7 @@ class Solution {
     }
     public int climbStairs(int n) {
         int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
         return countWays(n,dp);     
     }
 }
