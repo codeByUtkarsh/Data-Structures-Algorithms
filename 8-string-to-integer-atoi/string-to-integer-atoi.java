ae1c8b58900eaf9065fcb820aaeec249bdd26848
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2024-08-06 22:55:47 +0530

    Time: 1 ms (100.00%) | Memory: 42.3 MB (47.29%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,24 +1,31 @@
 class Solution {
     public int myAtoi(String s) {
-        if(s==null) return 0;
-        s= s.trim();
-
         if(s.length()==0) return 0;
-        int sign = +1;
-        long ans=0;
-
-        if(s.charAt(0)== '-') sign=-1;
-
-        int max = Integer.MAX_VALUE,min = Integer.MIN_VALUE;
-        int i = (s.charAt(0) == '+' || s.charAt(0)== '-') ? 1:0;
-        while(i<s.length()){
-            if(s.charAt(i)== ' ' || !Character.isDigit(s.charAt(i))) break;
-            ans = ans * 10 + s.charAt(i)-'0';
-
-            if(sign == -1 && -1*ans<min) return min;
-            if(sign == +1 && ans>max) return max;
-            i++; 
+        
+        int i=0;
+        while(i<s.length() && s.charAt(i)==' '){
+            i++;
         }
+        if (i == s.length()) return 0;
+        s = s.substring(i);
+        int sign = 1;
+        long ans =0;
+
+        if(s.charAt(0)=='-') sign = -1;
+
+        int min = Integer.MIN_VALUE ,max = Integer.MAX_VALUE;
+        i = (s.charAt(0)=='+' || s.charAt(0)=='-')?1:0;
+
+        while(i<s.length()){
+            if(s.charAt(i)==' ' || !Character.isDigit(s.charAt(i))) break;
+
+            ans = ans*10 + s.charAt(i) -'0';
+            if(sign == -1 && -1*ans<min) return min;
+            if(sign == 1 && ans>max) return max;
+            i++;
+        }
+
         return (int)(sign*ans);
+
     }
 }
