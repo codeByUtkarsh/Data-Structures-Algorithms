afde3959d89c61b5514b0cb6aee6e0ad8b3d6591
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2025-01-15 15:19:58 +0530

    Time: 9 ms (93.55%), Space: 48.6 MB (56.23%) - LeetHub

---
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -0,0 +1,52 @@
+class Pair{
+    int first;
+    int second;
+    public Pair(int first,int second){
+        this.first = first;
+        this.second = second;
+    }
+}
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        int src =k;
+        List<List<Pair>> adj = new ArrayList<>();
+        Queue<Pair> q = new LinkedList<>();
+        for(int i=0;i<=n;i++){
+            adj.add(new ArrayList<>());
+        }
+        int[] dist = new int[n+1];
+        for(int i=0;i<times.length;i++){
+            int u = times[i][0];
+            int v = times[i][1];
+            int wt = times[i][2];
+            adj.get(u).add(new Pair(wt,v));
+        }
+        for(int i=0;i<n+1;i++){
+            dist[i]=(int)(1e9);
+        }
+        dist[src]=0;
+        q.add(new Pair(0,src));
+        while(!q.isEmpty()){
+            Pair curr = q.poll();
+            int upDis = curr.first;
+            int upNode = curr.second;
+            for(Pair it:adj.get(upNode)){
+                int currDis = it.first;
+                int currNode = it.second;
+                int total = upDis+currDis;
+                if((total)<dist[currNode]){
+                    dist[currNode]=total;
+                    q.add(new Pair(total,currNode));
+                }
+            }
+        }
+        int minDis = -1;
+        for(int i=1;i<=n;i++){
+            if(dist[i]==(int)(1e9)){
+                return -1;
+            }
+            minDis = Math.max(minDis,dist[i]);
+        }
+        return minDis;
+    }
+}
