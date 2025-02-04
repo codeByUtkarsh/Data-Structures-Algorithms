14a6888f4cda449db4b49c2a0c60926f2d08e990
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2025-02-04 13:08:18 +0530

    Time: 0 ms (100%), Space: 43 MB (35.51%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
@@ -5,8 +5,8 @@ class Solution {
         while(low<=high){
             int mid = low+(high-low)/2;
             missing = arr[mid]-(mid+1);
-            if(missing<k) low=mid+1;
-            else high=mid-1;
+            if(missing>=k) high=mid-1;
+            else low=mid+1;
         }
         return low+k;
     }
