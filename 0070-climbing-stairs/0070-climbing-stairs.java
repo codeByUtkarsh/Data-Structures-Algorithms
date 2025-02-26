c8381e643bb1046717da53585dbbb40d8a91e37d
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2025-02-26 11:52:02 +0530

    Time: 0 ms (100%), Space: 40.3 MB (80.67%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,12 @@
 class Solution {
-    private int countWays(int ind , int[] dp){
-        if(ind==0) return 1;
-        if(ind<0) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int left = countWays(ind-1,dp);
-        int right = countWays(ind-2,dp);
-        return dp[ind] = left+right;
-    }
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return countWays(n,dp);
+        int prev2 = 1;
+        int prev = 1;
+        for(int i=2;i<=n;i++){
+            int curri = prev+prev2;
+            prev2 = prev;
+            prev = curri;
+        }
+        return prev;
     }
 }
