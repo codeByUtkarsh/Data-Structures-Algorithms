23abc52a95eae6ec4b5516e434b688a2fa651230
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2025-02-28 15:53:20 +0530

    Time: 0 ms (100%), Space: 40.9 MB (75.36%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findMaxRob(int ind,int[] nums,int[] dp){
+        if(ind == 0) return nums[ind];
+        if(ind<0) return 0;
+        if(dp[ind]!=-1) return dp[ind];
+        int pick = nums[ind] + findMaxRob(ind-2,nums,dp);
+        int notPick = findMaxRob(ind-1,nums,dp);
+        return dp[ind] = Math.max(pick,notPick);
+     }
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return findMaxRob(n-1,nums,dp);
+    }
+}
