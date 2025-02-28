996b003fd9cc42bab3e6c3d64fd89ffc0d02caab
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2025-02-28 17:31:35 +0530

    Time: 0 ms (100%), Space: 41.5 MB (18.25%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,10 +1,10 @@
 class Solution {
-    public int findMaxRob(List<Integer> nums){
-        int n = nums.size();
-        int prev = nums.get(0);
+    public int findMaxRob(int[] nums){
+        int n = nums.length;
+        int prev = nums[0];
         int prev2 = 0;
         for(int i=1;i<n;i++){
-            int take = nums.get(i) + prev2;
+            int take = nums[i] + prev2;
             int notTake = 0 + prev;
             int curri = Math.max(take,notTake);
             prev2 = prev;
@@ -15,11 +15,6 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         if(n==1) return nums[0];
-        List<Integer> list1 = new ArrayList<>(), list2 = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if(i!=0) list1.add(nums[i]);
-            if(i!=n-1) list2.add(nums[i]);
-        }
-        return Math.max(findMaxRob(list1),findMaxRob(list2));
+        return Math.max(findMaxRob(Arrays.copyOfRange(nums,0,n-1)),findMaxRob(Arrays.copyOfRange(nums,1,n)));
     }
 }
