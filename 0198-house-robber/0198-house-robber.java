d0e07e0da1240f50abd0208f2cb78cd7ae417552
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2025-02-28 16:32:33 +0530

    Time: 0 ms (100%), Space: 41 MB (75.36%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,8 +1,6 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp,-1);
         int prev = nums[0];
         int prev2 = 0;
         for(int i=1;i<n;i++){
