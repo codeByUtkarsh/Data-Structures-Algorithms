050202dea2cc42db89963410bc8bfe8c2fca1c37
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2025-03-05 00:10:43 +0530

    Time: 0 ms (100%), Space: 40.4 MB (59.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -4,9 +4,9 @@ class Solution {
         for(int i=0;i<m;i++){
             int[] temp = new int[n];
             for(int j=0;j<n;j++){
-                int left=0;
                 if(i==0 && j==0) temp[0]=1;
                 else{
+                    int left=0;
                     if(j>0) left = temp[j-1];
                     temp[j] = dp[j]+left;
                 }
