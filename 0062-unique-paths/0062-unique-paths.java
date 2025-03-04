97a4ad1b02010edd75d0a6af6c2ed455a77b9a5a
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2025-03-04 23:36:54 +0530

    Time: 0 ms (100%), Space: 40.6 MB (35.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,17 +1,18 @@
 class Solution {
-    public int calUP(int i,int j , int[][] dp){
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int up = calUP(i-1,j,dp);
-        int left = calUP(i,j-1,dp);
-        return dp[i][j] = up+left;
-    }
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int up=0;
+                int left=0;
+                if(i==0 && j==0) dp[0][0]=1;
+                else{
+                    if(i>0) up = dp[i-1][j];
+                    if(j>0) left = dp[i][j-1];
+                    dp[i][j] = up+left;
+                }
+            }
         }
-        return calUP(m-1,n-1,dp);
+        return dp[m-1][n-1];
     }
 }
