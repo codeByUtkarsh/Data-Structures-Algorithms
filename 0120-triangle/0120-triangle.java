0128b877a561d7666250fa61a027020441ae5221
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2025-03-05 18:27:13 +0530

    Time: 1 ms (99.79%), Space: 44.4 MB (35.42%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,19 +1,17 @@
 class Solution {
+    public int findMinPath(int i,int j,List<List<Integer>> triangle,int[][] dp,int n){
+        if(i==n-1) return triangle.get(n-1).get(j);
+        if(dp[i][j]!=-1) return dp[i][j];
+        int down = triangle.get(i).get(j) + findMinPath(i+1,j,triangle,dp,n);
+        int dg = triangle.get(i).get(j) + findMinPath(i+1,j+1,triangle,dp,n);
+        return dp[i][j] = Math.min(down,dg);
+    }
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.get(triangle.size()-1).size();
-        int[] dp = new int[n];
-        for(int i=0;i<n;i++){
-            dp[i] = triangle.get(n-1).get(i);
+        int[][] dp = new int[n][n];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
         }
-        for(int i=n-2;i>=0;i--){
-            int[] temp = new int[n];
-            for(int j=i;j>=0;j--){
-                int down = triangle.get(i).get(j)+dp[j];
-                int diagonal = triangle.get(i).get(j) + dp[j+1];
-                temp[j] = Math.min(down,diagonal);
-            }
-            dp = temp;
-        }
-        return dp[0];
+       return findMinPath(0,0,triangle,dp,n);
     }
 }
