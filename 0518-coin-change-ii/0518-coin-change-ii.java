cb063c5e6a7769204cd64dcfcbd24cb2d3283406
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2025-03-21 19:22:14 +0530

    Time: 8 ms (69.73%), Space: 49.5 MB (23.19%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -12,9 +12,18 @@ class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
         int[][] dp =new int[n][amount+1];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        for(int target=0;target<=amount;target++){
+            if(target%coins[0]==0) dp[0][target]=1;
+            else dp[0][target]=0;
         }
-        return findWays(n-1,coins,amount,dp);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                int notTake = dp[i-1][j];
+        int take =0;
+        if(coins[i]<=j) take = dp[i][j-coins[i]];
+        dp[i][j]=(take+notTake);
+            }
+        }
+        return dp[n-1][amount];
     }
 }
