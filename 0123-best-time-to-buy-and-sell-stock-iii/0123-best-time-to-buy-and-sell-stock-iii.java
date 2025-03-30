1d402e51748f19349247a6772be44303468486b0
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2025-03-31 02:17:07 +0530

    Time: 56 ms (43.62%), Space: 64.4 MB (30.81%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,32 +1,22 @@
 class Solution {
-    public int findMP(int day, int buy, int[] prices, int cap,int[][][] dp) {
-        if (cap == 0)
-            return 0;
-        if (day == prices.length)
-            return 0;
-        if (dp[day][buy][cap] != -1)
-            return dp[day][buy][cap];
-        int profit = 0;
-        if (buy == 1) {
-            int buyy = -prices[day] + findMP(day + 1, 0, prices, cap,dp);
-            int notBuy = findMP(day + 1, 1, prices, cap,dp);
-            profit = Math.max(buyy, notBuy);
-        } else {
-            int sell = prices[day] + findMP(day + 1, 1, prices, cap - 1,dp);
-            int notSell = findMP(day + 1, 0, prices, cap,dp);
-            profit = Math.max(sell, notSell);
-        }
-        return dp[day][buy][cap] = profit;
-    }
-
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n][2][3];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 2; j++) {
-                Arrays.fill(dp[i][j], -1);
+        int[][][] dp = new int[n + 1][2][3];
+        for (int day = n - 1; day >= 0; day--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= 2; cap++) {
+                    if (buy == 1) {
+                        int buyy = -prices[day] + dp[day + 1][0][cap];
+                        int notBuy = dp[day + 1][1][cap];
+                        dp[day][buy][cap] = Math.max(buyy, notBuy);
+                    } else {
+                        int sell = prices[day] + dp[day+1][1][cap-1];
+                        int notSell = dp[day + 1][0][cap];
+                        dp[day][buy][cap]= Math.max(sell, notSell);
+                    }
+                }
             }
         }
-        return findMP(0, 1, prices, 2,dp);
+        return dp[0][1][2];
     }
 }
