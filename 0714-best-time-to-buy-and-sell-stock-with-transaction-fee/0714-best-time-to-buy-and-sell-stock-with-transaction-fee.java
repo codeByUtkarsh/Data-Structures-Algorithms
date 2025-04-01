20eaaff9f11e08e1e746fd97b8fb6c820ad02c9f
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2025-04-02 00:40:07 +0530

    Time: 16 ms (51.2%), Space: 55.3 MB (56.26%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,19 +1,16 @@
 class Solution {
-    public int findMP(int day,int buy,int n,int f,int[] prices,int[][] dp ){
-        if(day==n) return 0;
-        if(dp[day][buy]!=-1) return dp[day][buy];
-        if(buy==1){
-            return dp[day][buy]=Math.max(-prices[day]-f+findMP(day+1,0,n,f,prices,dp),findMP(day+1,1,n,f,prices,dp));
-        }else{
-            return dp[day][buy]= Math.max(prices[day]+findMP(day+1,1,n,f,prices,dp),findMP(day+1,0,n,f,prices,dp));
-        }
-    }
     public int maxProfit(int[] prices, int fee) {
         int n = prices.length;
-        int[][] dp = new int[n][2];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        int[][] dp = new int[n + 1][2];
+        for (int day = n - 1; day >= 0; day--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                if (buy == 1) {
+                    dp[day][buy] = Math.max(-prices[day]+ dp[day + 1][0],dp[day + 1][1]);
+                } else {
+                    dp[day][buy] = Math.max(prices[day]- fee + dp[day + 1][1],dp[day + 1][0]);
+                }
+            }
         }
-        return findMP(0,1,n,fee,prices,dp);
+        return dp[0][1];
     }
 }
