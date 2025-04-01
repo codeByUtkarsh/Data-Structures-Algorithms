f1707e06d27b1433e5661327dfbc53fc58dea7c8
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2025-04-01 23:04:00 +0530

    Time: 2 ms (80.27%), Space: 43.3 MB (56.49%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,19 +1,16 @@
 class Solution {
-    public int findMP(int ind,int trans,int k,int n,int[] prices,int[][] dp){
-        if(ind==n || trans==2*k) return 0;
-        if(dp[ind][trans]!=-1) return dp[ind][trans];
-        if(trans%2==0){
-            return dp[ind][trans] = Math.max(-prices[ind]+findMP(ind+1,trans+1,k,n,prices,dp),findMP(ind+1,trans,k,n,prices,dp));
-        }else{
-            return dp[ind][trans] = Math.max(prices[ind]+findMP(ind+1,trans+1,k,n,prices,dp),findMP(ind+1,trans,k,n,prices,dp));
-        }
-    }
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
-        int[][] dp = new int[n][2*k+1];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        int[][] dp = new int[n + 1][2 * k + 1];
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int trans = 2 * k - 1; trans >= 0; trans--) {
+                if (trans % 2 == 0) {
+                    dp[ind][trans] = Math.max(-prices[ind] + dp[ind + 1][trans + 1],dp[ind + 1][trans]);
+                } else {
+                    dp[ind][trans] = Math.max(prices[ind] + dp[ind + 1][trans + 1],dp[ind + 1][trans]);
+                }
+            }
         }
-        return findMP(0,0,k,n,prices,dp);
+        return dp[0][0];
     }
 }
