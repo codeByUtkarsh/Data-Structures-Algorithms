3d6977f596c9731ae60893b3c975f7360da5a84d
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2025-04-05 02:44:50 +0530

    Time: 426 ms (5.02%) | Memory: 79.8 MB (7.21%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,37 +1,19 @@
 class Solution {
-    public int lis(int[]nums , int[] arr2){
-        int n = nums.length;
-        int m = arr2.length;
-
-        int[][] dp = new int[n+1][m+1];
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<m+1;j++){
-                //valid condition
-                if(nums[i-1]== arr2[j-1]){
-                    dp[i][j]= dp[i-1][j-1]+1;
-                }else{
-                    int ans1 = dp[i-1][j];
-                    int ans2 = dp[i][j-1];
-                    dp[i][j] = Math.max(ans1,ans2);
-                }
-            }
+    public int lis(int ind,int prev,int n,int[] nums,int[][] dp){
+        if(ind==n) return 0;
+        if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
+        int len = lis(ind+1,prev,n,nums,dp);
+        if(prev==-1 || nums[ind]>nums[prev]){
+            len = Math.max(len,1+lis(ind+1,ind,n,nums,dp));
         }
-        return dp[n][m];
+        return dp[ind][prev+1] =  len;
     }
     public int lengthOfLIS(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            set.add(nums[i]);
+        int n = nums.length;
+        int[][] dp = new int[n][n+1];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
         }
-
-        int[] arr2 = new int[set.size()];
-        int i=0;
-        for(int num:set){
-            arr2[i]=num;
-            i++;
-        }
-        Arrays.sort(arr2);
-
-        return lis(nums,arr2);
+        return lis(0,-1,n,nums,dp);
     }
 }
